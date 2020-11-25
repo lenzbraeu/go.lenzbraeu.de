a2341a2e64fe6e6a1cc32d2d477fb6d36d640b12
erdii <me@erdii.engineering>
2020-11-25 19:30:47 +0100

    initial

---
+++ cmd/render/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+)
+
+const (
+	outDir    = "docs/"
+	domain    = "go.lenzbraeu.de"
+	githubOrg = "lenzbraeu"
+)
+
+// Add new go modules here and
+// then execute `go run ./cmd/render`
+var modules = []string{
+	"modbus2",
+	"modbusclients",
+}
+
+var (
+	moduleTemplateString = `<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<!-- Go Imports -->
+	<meta name="go-import" content="{{ .GoImport }}">
+	<meta name="go-source" content="{{ .GoSource }}">
+	<meta http-equiv="refresh" content="0; {{ .Redirect }}">
+</head>
+<body>
+</body>
+</html>`
+
+	indexTemplateString = `<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta http-equiv="refresh" content="0; {{ .Redirect }}">
+</head>
+<body>
+</body>
+</html>`
+)
+
+type moduleTemplateData struct {
+	GoImport string
+	GoSource string
+	Redirect string
+}
+
+type indexTemplateData struct {
+	Redirect string
+}
+
+func main() {
+	// Load templates
+	moduleTemplate := template.Must(
+		template.New("module page").Parse(moduleTemplateString),
+	)
+	indexTemplate := template.Must(
+		template.New("index page").Parse(indexTemplateString),
+	)
+
+	// Remove output folder
+	err := os.RemoveAll(outDir)
+	if err != nil {
+		panic(fmt.Sprintf("Could not remove outDir: %s %v", outDir, err))
+	}
+
+	// Recreate output folder
+	err = os.MkdirAll(outDir, os.ModePerm)
+	if err != nil {
+		panic(fmt.Sprintf("Could not recreate outDir: %s %v", outDir, err))
+	}
+
+	// Create CNAME file in output folder
+	cnameFilePath := filepath.Join(outDir, "CNAME")
+	file, err := os.Create(cnameFilePath)
+	defer file.Close()
+	if err != nil {
+		panic(fmt.Sprintf("could not create cname file: %v", err))
+	}
+	fmt.Fprintln(file, domain)
+
+	// Create subfolder and page for each module
+	for _, module := range modules {
+		subfolderPath := filepath.Join(outDir, module)
+		pagePath := filepath.Join(subfolderPath, "index.html")
+
+		// Create module subfolder
+		err = os.Mkdir(subfolderPath, os.ModePerm)
+		if err != nil {
+			panic(fmt.Sprintf("Could not create subfolder for module: %s %s %v", module, subfolderPath, err))
+		}
+
+		// Create module page file
+		file, err := os.Create(pagePath)
+		defer file.Close()
+		if err != nil {
+			panic(fmt.Sprintf("could not create module page file: %v", err))
+		}
+
+		// Render template into page file
+		moduleTemplate.Execute(file, moduleTemplateData{
+			GoImport: fmt.Sprintf("%s/%s git https://github.com/%s/%s", domain, module, githubOrg, module),
+			GoSource: fmt.Sprintf(
+				`%s/%s _ https://github.com/%s/%s/tree/main{/dir} https://github.com/%s/%s/blob/main{/dir}/{file}#L{line}`,
+				domain,
+				module,
+				githubOrg,
+				module,
+				githubOrg,
+				module,
+			),
+			Redirect: fmt.Sprintf("https://godoc.org/%s/%s", domain, module),
+		})
+	}
+
+	// Create index file
+	indexFilePath := filepath.Join(outDir, "index.html")
+	file, err = os.Create(indexFilePath)
+	defer file.Close()
+	if err != nil {
+		panic(fmt.Sprintf("could not create index file: %v", err))
+	}
+
+	// Render template into index file
+	indexTemplate.Execute(file, indexTemplateData{
+		Redirect: fmt.Sprintf("https://github.com/%s", githubOrg),
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go.lenzbraeu.de/go.lenzbraeu.de
+
+go 1.15
